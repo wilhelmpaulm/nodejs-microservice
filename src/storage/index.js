0dac8caa98c18513a2d7b5c5443588f6f40d240a
wilhelmpaulm <wilhelmpaulm@gmail.com>
2022-01-16 15:11:57 -0800

    feat: setup for DB

---
+++ src/storage/index.js
@@ -0,0 +1,17 @@
+const booksDbFactory = require('./books-db.js')
+const { MongoClient } = require("mongodb");
+
+console.log(process.env.DB_URI);
+const uri = process.env.DB_URI;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const dbFactory = async () => {
+  await client.connect();
+  return client.db("dev");
+};
+
+const booksDb = booksDbFactory({ dbFactory });
+module.exports = booksDb;
