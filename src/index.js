044520be89a4ddf840443c9d4281faa7994ac7e8
wilhelmpaulm <wilhelmpaulm@gmail.com>
2022-01-16 15:56:23 -0800

    chore: removed trash console.log lines

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ dotenv.config();
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const { createBook, updateBook, getBook, deleteBook } = require("./controllers");
-const { bookFactory } = require("./entities");
-const { generateId } = require("./helpers");
-const { findAll, findOne } = require("./storage");
 
 const app = express();
 
@@ -18,9 +14,6 @@ app.use(cookieParser());
 
 // listen for requests
 app.listen(8088, async () => {
-  console.log(await getBook({ id: "ckyhw6ryy000220ti6yg56yu5" }));
-  console.log(await deleteBook({ id: "ckyhw6ryy000220ti6yg56yu5" }));
-  console.log(await getBook({ id: "ckyhw6ryy000220ti6yg56yu5" }));
   console.log("Server is listening on port 8088");
 });
 
